73266e9f7b1142f0887dba63098a6ed441df40d8
Manuel Lajo <manuel.lajo@autodesk.com>
2020-02-10 03:47:45 -0500

    update Toolbar.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,14 +8,12 @@ import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <DrawerToggle clicked={props.drawerToggleClicked} />
-    <div>MENU</div>
     <div className={classes.Logo}>
-        <Logo />
+      <Logo />
     </div>
     <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
-    {/* <div> LOGO</div> */}
   </header>
 );
 
